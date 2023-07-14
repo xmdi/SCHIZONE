cef50ac2e62ff54ad0c6795fba54fe050bfca30c
xmdi <xmdi@protonmail.com>
2023-07-14 15:08:52 -0400

    added L1Qs idea

--- ex/ex008_randomness/README.md
+++ ex/ex008_randomness/README.md
@@ -10,3 +10,5 @@ In [ex008d](ex008d_random_float_array), we generate an array of random floats.
 
 In [ex008e](ex008e_deal_or_no_deal), we recreate the game show "Deal or No Deal" using random integers.
 
+In [ex008f](ex008f_cpu_support_and_getrandom_syscall), we use the `cpuid` instruction to test for cpu support of the `rdrand` and `rdseed` instructions. We also implement an alternative using the `GETRANDOM` syscall, although it is nonstandard on FreeBSD.
+
---
+++ ex/ex008_randomness/ex008f_cpu_support_and_getrandom_syscall/code.asm
@@ -0,0 +1,187 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;DEFINITIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%define LOAD_ADDRESS 0x00020000 ; pretty much any number >0 works
+%define CODE_SIZE END-(LOAD_ADDRESS+0x78) ; everything beyond HEADER is code
+%define PRINT_BUFFER_SIZE 4096
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;HEADER;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+BITS 64
+org LOAD_ADDRESS
+ELF_HEADER:
+	db 0x7F,"ELF" ; magic number to indicate ELF file
+	db 0x02 ; 0x1 for 32-bit, 0x2 for 64-bit
+	db 0x01 ; 0x1 for little endian, 0x2 for big endian
+	db 0x01 ; 0x1 for current version of ELF
+	db 0x09 ; 0x9 for FreeBSD, 0x3 for Linux (doesn't seem to matter)
+	db 0x00 ; ABI version (ignored?)
+	times 7 db 0x00 ; 7 padding bytes
+	dw 0x0002 ; executable file
+	dw 0x003E ; AMD x86-64 
+	dd 0x00000001 ; version 1
+	dq START ; entry point for our program
+	dq 0x0000000000000040 ; 0x40 offset from ELF_HEADER to PROGRAM_HEADER
+	dq 0x0000000000000000 ; section header offset (we don't have this)
+	dd 0x00000000 ; unused flags
+	dw 0x0040 ; 64-byte size of ELF_HEADER
+	dw 0x0038 ; 56-byte size of each program header entry
+	dw 0x0001 ; number of program header entries (we have one)
+	dw 0x0000 ; size of each section header entry (none)
+	dw 0x0000 ; number of section header entries (none)
+	dw 0x0000 ; index in section header table for section names (waste)
+PROGRAM_HEADER:
+	dd 0x00000001 ; 0x1 for loadable program segment
+	dd 0x00000007 ; read/write/execute flags
+	dq 0x0000000000000078 ; offset of code start in file image (0x40+0x38)
+	dq LOAD_ADDRESS+0x78 ; virtual address of segment in memory
+	dq 0x0000000000000000 ; physical address of segment in memory (ignored?)
+	dq CODE_SIZE ; size (bytes) of segment in file image
+	dq CODE_SIZE+PRINT_BUFFER_SIZE ; size (bytes) of segment in memory
+	dq 0x0000000000000000 ; alignment (doesn't matter, only 1 segment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INCLUDES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
+
+%include "lib/io/print_chars.asm"
+; void print_chars(int {rdi}, char* {rsi}, int {rdx});
+
+%include "lib/io/print_int_d.asm"
+; void print_int_d(int {rdi}, int {rsi});
+
+%include "lib/sys/exit.asm"	
+; void exit(byte {dil});
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+START:
+
+	mov rdi,SYS_STDOUT
+
+	; test if rdrand is supported
+.test_for_rdrand:
+	mov rax,1	; standard function 1: "Processor Info & Feature Bits"
+	cpuid
+	
+	test rcx,1<<30	; bit 30 indicates whether or not rdrand is supported
+	jz .rdrand_unsupported
+
+.rdrand_supported:
+	; print that rdrand was supported
+	mov rsi,.grammar
+	mov rdx,26
+	call print_chars
+
+	; generate a random number using rdrand
+.loop1:
+	rdrand rsi
+	jnc .loop1
+
+	; print the random number 
+	call print_int_d
+
+	jmp .test_for_rdseed
+
+.rdrand_unsupported:
+
+	; print that rdrand was NOT supported
+	mov rsi,.grammar+34
+	mov rdx,26
+	call print_chars
+
+	mov rsi,.grammar+18
+	mov rdx,6
+	call print_chars
+	
+	; test if rdseed is supported
+.test_for_rdseed:
+
+	; print newline
+	mov rsi,.grammar+60
+	mov rdx,2
+	call print_chars
+
+	mov rax,7	; standard function 7: "Extended Features"
+	xor rcx,rcx	; leaf 0 for standard function 7
+	cpuid
+	
+	test rbx,1<<18	; bit 18 indicates whether or not rdseed is supported
+	jz .rdseed_unsupported
+
+.rdseed_supported:
+
+	; print that rdseed was supported
+	mov rsi,.grammar
+	mov rdx,18
+	call print_chars
+
+	mov rsi,.grammar+26
+	mov rdx,8
+	call print_chars
+
+	; generate a random number using rdseed
+.loop2:
+	rdseed rsi
+	jnc .loop2
+
+	; print the random number 
+	call print_int_d
+
+	jmp .use_getrandom_syscall
+
+.rdseed_unsupported:
+
+	; print that rdseed was NOT supported
+	mov rsi,.grammar+34
+	mov rdx,26
+	call print_chars
+
+	mov rsi,.grammar+26
+	mov rdx,6
+	call print_chars
+
+.use_getrandom_syscall:
+
+	; print newline and GETRANDOM syscall statement
+	mov rsi,.grammar+60
+	mov rdx,27
+	call print_chars
+
+	mov rax,SYS_GETRANDOM		; syscall to get random data
+	mov rdi,.random_quadword	; where to put random data
+	mov rsi,8			; number of bytes
+	xor rdx,rdx			; flags
+	syscall
+
+	mov rdi,SYS_STDOUT
+	mov rsi,[.random_quadword]
+	call print_int_d
+
+	; print newline
+	mov rsi,.grammar+60
+	mov rdx,2
+	call print_chars
+
+	call print_buffer_flush
+
+	xor dil,dil
+	call exit
+	
+.random_quadword:
+	dq 0
+
+.grammar:
+	db `This CPU supports RDRAND: RDSEED: This CPU does not support .\n`
+	db `Using GETRANDOM syscall: `
+END:
+
+PRINT_BUFFER: 	; PRINT_BUFFER_SIZE bytes will be allocated here at runtime,
+		; all initialized to zeros
