742b206c8997591451eb7511e4406a5b6aa00f29
xmdi <xmdi@protonmail.com>
2023-08-28 22:52:15 -0400

    still bugs

---
+++ ex/ex013_parsing_input_files/ex013c_multiple_input_files/code.asm
@@ -0,0 +1,153 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;DEFINITIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%define LOAD_ADDRESS 0x00020000 ; pretty much any number >0 works
+%define CODE_SIZE END-(LOAD_ADDRESS+0x78) ; everything beyond HEADER is code
+%define PRINT_BUFFER_SIZE 4096
+%define READ_BUFFER_SIZE 64
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;HEADER;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+BITS 64
+org LOAD_ADDRESS
+ELF_HEADER:
+	db 0x7F,"ELF" ; magic number to indicate ELF file
+	db 0x02 ; 0x1 for 32-bit, 0x2 for 64-bit
+	db 0x01 ; 0x1 for little endian, 0x2 for big endian
+	db 0x01 ; 0x1 for current version of ELF
+	db 0x09 ; 0x9 for FreeBSD, 0x3 for Linux (doesn't seem to matter)
+	db 0x00 ; ABI version (ignored?)
+	times 7 db 0x00 ; 7 padding bytes
+	dw 0x0002 ; executable file
+	dw 0x003E ; AMD x86-64 
+	dd 0x00000001 ; version 1
+	dq START ; entry point for our program
+	dq 0x0000000000000040 ; 0x40 offset from ELF_HEADER to PROGRAM_HEADER
+	dq 0x0000000000000000 ; section header offset (we don't have this)
+	dd 0x00000000 ; unused flags
+	dw 0x0040 ; 64-byte size of ELF_HEADER
+	dw 0x0038 ; 56-byte size of each program header entry
+	dw 0x0001 ; number of program header entries (we have one)
+	dw 0x0000 ; size of each section header entry (none)
+	dw 0x0000 ; number of section header entries (none)
+	dw 0x0000 ; index in section header table for section names (waste)
+PROGRAM_HEADER:
+	dd 0x00000001 ; 0x1 for loadable program segment
+	dd 0x00000007 ; read/write/execute flags
+	dq 0x0000000000000078 ; offset of code start in file image (0x40+0x38)
+	dq LOAD_ADDRESS+0x78 ; virtual address of segment in memory
+	dq 0x0000000000000000 ; physical address of segment in memory (ignored?)
+	dq CODE_SIZE ; size (bytes) of segment in file image
+	dq CODE_SIZE+PRINT_BUFFER_SIZE+READ_BUFFER_SIZE ; size (bytes) of 
+							; segment in memory
+	dq 0x0000000000000000 ; alignment (doesn't matter, only 1 segment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INCLUDES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
+%include "lib/io/file_open.asm"
+; int {rax} file_open(char* {rdi}, int {rsi}, int {rdx});
+
+%include "lib/io/parse_delimited_float_file.asm"
+; void parse_delimited_float_file(double* {rdi}, uint {rsi}, uint {rdx},
+;		 uint {rcx}, void* {r8}, char {r9b});
+
+%include "lib/io/print_array_float.asm"
+; void print_array_float(int {rdi}, double* {rsi}, int {rdx}, int {rcx}, 
+;	int {r8}, void* {r9}, int {r10});
+
+%include "lib/math/matrix/matrix_multiply.asm"
+; void matrix_multiply(double* {rdi}, double* {rsi}, double* {rdx}, uint {rcx}
+;	uint {r8}, uint {r9});
+
+%include "lib/sys/exit.asm"	
+; void exit(byte {dil});
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+START:
+
+	; open input file 1
+	mov rdi,.filename1
+	mov rsi,SYS_READ_ONLY
+	mov rdx,SYS_DEFAULT_PERMISSIONS
+	call file_open	; file descriptor in {rax}
+
+	; read floats from csv into array using READ_BUFFER
+	mov rdi,.array1
+	mov rsi,rax
+	mov rdx,3*3
+	mov rcx,READ_BUFFER_SIZE
+	mov r8,READ_BUFFER
+	mov r9,","
+	call parse_delimited_float_file
+
+	; open input file 2
+	mov rdi,.filename1
+	mov rsi,SYS_READ_ONLY
+	mov rdx,SYS_DEFAULT_PERMISSIONS
+	call file_open	; file descriptor in {rax}
+
+	; read floats from csv into array using READ_BUFFER
+	mov rdi,.array2
+	mov rsi,rax
+	mov rdx,3*3
+	mov rcx,READ_BUFFER_SIZE
+	mov r8,READ_BUFFER
+	mov r9,","
+	call parse_delimited_float_file
+
+
+
+
+	; compute product of input matrices
+	mov rdi,.product
+	mov rsi,.array1
+	mov rdx,.array2
+	mov rcx,3
+	mov r8,3
+	mov r9,3
+	call matrix_multiply
+
+	; print out array
+	mov rdi,SYS_STDOUT
+	mov rsi,.product
+	mov rdx,3
+	mov rcx,3
+	xor r8,r8
+	mov r9,print_float
+	mov r10,6
+	call print_array_float
+
+	; flush print buffer
+	call print_buffer_flush
+
+	; exit
+	xor dil,dil
+	call exit	
+
+.filename1:
+	db `multiply.this`,0
+.filename2:
+	db `by.this`,0
+
+.array1:; space for 3x3 element float matrix
+	times 3*3 dq 0
+.array2:; space for 3x3 element float matrix
+	times 3*3 dq 0
+.product: ; space for 3x3 element float product matrix
+	times 3*3 dq 0
+
+END:
+
+PRINT_BUFFER: 	; PRINT_BUFFER_SIZE bytes will be allocated here at runtime,
+		; all initialized to zeros
+
+READ_BUFFER equ (PRINT_BUFFER+PRINT_BUFFER_SIZE)
