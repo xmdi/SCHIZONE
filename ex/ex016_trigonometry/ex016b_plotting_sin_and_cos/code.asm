e29640754da0b2aba3d1fb65cecdc094805a4fa6
xmdi <xmdi@protonmail.com>
2023-10-08 19:58:54 -0400

    prep ex16b

--- ex/ex016_trigonometry/ex016b_plotting_sin_and_cos/code.asm
+++ ex/ex016_trigonometry/ex016b_plotting_sin_and_cos/code.asm
@@ -5,6 +5,7 @@
 %define LOAD_ADDRESS 0x00020000 ; pretty much any number >0 works
 %define CODE_SIZE END-(LOAD_ADDRESS+0x78) ; everything beyond HEADER is code
 %define PRINT_BUFFER_SIZE 4096
+%define HEAP_SIZE 2048
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;HEADER;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -40,7 +41,7 @@ PROGRAM_HEADER:
 	dq LOAD_ADDRESS+0x78 ; virtual address of segment in memory
 	dq 0x0000000000000000 ; physical address of segment in memory (ignored?)
 	dq CODE_SIZE ; size (bytes) of segment in file image
-	dq CODE_SIZE+PRINT_BUFFER_SIZE ; size (bytes) of segment in memory
+	dq CODE_SIZE+PRINT_BUFFER_SIZE+HEAP_SIZE ; size (bytes) of segment in memory
 	dq 0x0000000000000000 ; alignment (doesn't matter, only 1 segment)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -60,6 +61,28 @@ PROGRAM_HEADER:
 
 %include "lib/io/print_float.asm"
 ; void print_float(int {rdi}, double {xmm0}, int {rsi});
+
+%include "lib/io/file_open.asm"
+; int {rax} file_open(char* {rdi}, int {rsi}, int {rdx});
+
+%include "lib/io/file_close.asm"
+; int {rax} file_close(int {rdi});
+
+%include "lib/io/svg/scatter_plot.asm"
+; void scatter_plot(uint {rdi}, struct* {rsi});
+
+%include "lib/math/parametric/evaluate_parameters.asm"
+; bool {rax} evaluate_parameters(void* {rdi}, void* {rsi}, void* {rdx});
+
+%include "lib/math/parametric/linear_space.asm"
+; bool {rax} linear_space(double* {rdi}, long {rsi}, ulong {rdx}
+;			double {xmm0}, double {xmm1});
+
+%include "lib/mem/heap_init.asm"
+; void heap_init(void);
+
+%include "lib/mem/heap_alloc.asm"
+; void* {rax} heap_alloc(long {rdi});
 
 %include "lib/sys/exit.asm"	
 ; void exit(byte {dil});
@@ -124,16 +147,102 @@ START:
 	call exit	
 
 
-.number:
-	dq 3.14159
-.tolerance:
-	dq 0.0001
-.grammar1:
-	db `sine()=\n`
-.grammar2:
-	db `cosine()=\n`
+.x_param:
+	dq 0 ; address of next parameter in list (0 on last element)
+	dq 0 ; address of first parameter value
+	dq 0 ; extra stride between parameter values
+	dq 101 ; number of values (only matters for first input parameter in linked list)
+	dq 0 ; work zone (track address of current element), initial value unused
+
+.y_param:
+	dq 0 ; address of next parameter in list (0 on last element)
+	dq 0 ; address of first parameter value
+	dq 0 ; extra stride between parameter values
+	dq 101 ; number of values (only matters for first input parameter in linked list)
+	dq 0 ; work zone (track address of current element), initial value unused
+
+.filename:
+	db `func.svg\0`
+
+.title:
+	db `f(x)=x^3-3x^2-x+2\0`
+
+.xlabel:
+	db `x\0`
+
+.ylabel:
+	db `f(x)\0`
+
+.plot_structure:
+	dq .title; address of null-terminated title string {*+0}
+	dq .xlabel; address of null-terminated x-label string {*+8}
+	dq .ylabel; address of null-terminated y-label string {*+16}
+	dq .data; address of linked list for datasets {*+24}
+	dw 480; plot width (px) {*+32}
+	dw 200; plot height (px) {*+34}
+	dw 5; plot margins (px) {*+36}
+	dq -2.0; x-min (double) {*+38}
+	dq 4.0; x-max (double) {*+46}
+	dq -18.0; y-min (double) {*+54}
+	dq 18.0; y-max (double) {*+62}
+	dw 0; legend left x-coordinate (px) {*+70}
+	dw 0; legend top y-coordinate (px) {*+72}
+	dw 0; legend width (px) {*+74}
+	dd 0xFFFFFF; #XXXXXX RGB background color {*+76}
+	dd 0x000000; #XXXXXX RGB axis color {*+80}
+	dd 0x000000; #XXXXXX RGB font color {*+84}
+	db 7; number of major x-ticks {*+88}
+	db 7; number of major y-ticks {*+89}
+	db 0; minor subdivisions per x-tick {*+90}
+	db 0; minor subdivisions per y-tick {*+91}
+	db 2; significant digits on x values {*+92}
+	db 2; significant digits on y values {*+93}
+	db 14; title font size (px) {*+94}
+	db 5; vertical margin below title (px) {*+95}
+	db 12; axis label font size (px) {*+96}
+	db 8; tick & legend label font size (px) {*+97}
+	db 5; horizontal margin right of y-tick labels (px) {*+98}
+	db 5; vertical margin above x-tick labels (px) {*+99}
+	db 1; grid major stroke thickness (px) {*+100}
+	db 0; grid minor stroke thickness (px) {*+101}
+	db 30; width for y-axis ticks (px) {*+102}
+	db 30; height for x-axis ticks (px) {*+103}
+	db 0x1F; flags: {*+104}
+		; bit 0 (LSB)	= show title?
+		; bit 1		= show x-label?
+		; bit 2		= show y-label?
+		; bit 3		= draw grid?
+		; bit 4		= show tick labels?
+		; bit 5		= draw legend?
+
+.data:
+	dq 0; address of next dataset in linked list {*+0}
+	dq 0; address of null-terminated label string {*+8}
+	dq 0; address of first x-coordinate {*+16}
+	dw 0; extra stride between x-coord elements {*+24}
+	dq 0; address of first y-coordinate {*+26}
+	dw 0; extra stride between y-coord elements {*+34}
+	dd 101; number of elements {*+36}
+	dd 0x000000; #XXXXXX RGB marker color {*+40}
+	dd 0xFF0000; #XXXXXX RGB line color {*+44}
+	dd 0x000000; #XXXXXX RGB fill color {*+48}
+	db 1; marker size (px) {*+52}
+	db 2; line thickness (px) {*+53}
+	db 0; fill opacity (%) {*+54}
+	db 0x03; flags: {*+55}
+		; bit 0 (LSB)	= point marker?
+		; bit 1		= connecting lines?
+		; bit 2		= dashed line? (bit 1 must be set)
+		; bit 3		= fill?
+		; bit 4		= include in legend?
+		; bits 6-5	= 00 = no curves
+		;		= 01 = quadratic bezier
+		;		= 10 = cubic bezier
+		;		= 11 = arc
 
 END:
 
 PRINT_BUFFER: 	; PRINT_BUFFER_SIZE bytes will be allocated here at runtime,
 		; all initialized to zeros
+
+HEAP_START_ADDRESS equ (PRINT_BUFFER+PRINT_BUFFER_SIZE)
