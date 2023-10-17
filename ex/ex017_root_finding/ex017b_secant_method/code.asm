6a76a22909552c5dbdf8a558ee3781c3243031a0
xmdi <xmdi@protonmail.com>
2023-10-17 18:40:10 -0400

    change to secant

---
+++ ex/ex017_root_finding/ex017b_secant_method/code.asm
@@ -0,0 +1,115 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;DEFINITIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%define LOAD_ADDRESS 0x00020000 ; pretty much any number >0 works
+%define CODE_SIZE END-(LOAD_ADDRESS+0x78) ; everything beyond HEADER is code
+%define PRINT_BUFFER_SIZE 4096
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;HEADER;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+BITS 64
+org LOAD_ADDRESS
+ELF_HEADER:
+	db 0x7F,"ELF" ; magic number to indicate ELF file
+	db 0x02 ; 0x1 for 32-bit, 0x2 for 64-bit
+	db 0x01 ; 0x1 for little endian, 0x2 for big endian
+	db 0x01 ; 0x1 for current version of ELF
+	db 0x09 ; 0x9 for FreeBSD, 0x3 for Linux (doesn't seem to matter)
+	db 0x00 ; ABI version (ignored?)
+	times 7 db 0x00 ; 7 padding bytes
+	dw 0x0002 ; executable file
+	dw 0x003E ; AMD x86-64 
+	dd 0x00000001 ; version 1
+	dq START ; entry point for our program
+	dq 0x0000000000000040 ; 0x40 offset from ELF_HEADER to PROGRAM_HEADER
+	dq 0x0000000000000000 ; section header offset (we don't have this)
+	dd 0x00000000 ; unused flags
+	dw 0x0040 ; 64-byte size of ELF_HEADER
+	dw 0x0038 ; 56-byte size of each program header entry
+	dw 0x0001 ; number of program header entries (we have one)
+	dw 0x0000 ; size of each section header entry (none)
+	dw 0x0000 ; number of section header entries (none)
+	dw 0x0000 ; index in section header table for section names (waste)
+PROGRAM_HEADER:
+	dd 0x00000001 ; 0x1 for loadable program segment
+	dd 0x00000007 ; read/write/execute flags
+	dq 0x0000000000000078 ; offset of code start in file image (0x40+0x38)
+	dq LOAD_ADDRESS+0x78 ; virtual address of segment in memory
+	dq 0x0000000000000000 ; physical address of segment in memory (ignored?)
+	dq CODE_SIZE ; size (bytes) of segment in file image
+	dq CODE_SIZE+PRINT_BUFFER_SIZE ; size (bytes) of segment in memory
+	dq 0x0000000000000000 ; alignment (doesn't matter, only 1 segment)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INCLUDES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
+
+%include "lib/math/root_finding/secant_method.asm"
+; ulong {rax}, double {xmm0} bisection_method(void* {rdi}, double {xmm0}, 
+;				double {xmm1}, double {xmm2});
+
+%include "lib/io/print_float.asm"
+; void print_float(int {rdi}, double {xmm0}, int {rsi});
+
+%include "lib/io/print_chars.asm"
+; void print_chars(int {rdi}, char* {rsi}, int {rdx});
+
+%include "lib/sys/exit.asm"	
+; void exit(byte {dil});
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+SQRT_2:	; double {xmm0} SQRT_2({xmm0}).
+	mulsd xmm0,xmm0
+	subsd xmm0,[.two]
+	ret
+.two:
+	dq 2.0
+	
+START:
+
+	; use secant method to compute sqrt(2)
+	mov rdi,SQRT_2
+	movsd xmm0,[.lower_bound]
+	movsd xmm1,[.upper_bound]
+	movsd xmm2,[.tolerance]
+	call secant_method
+
+	; print result
+	mov rdi,SYS_STDOUT
+	mov rsi,.grammar
+	mov rdx,8
+	call print_chars
+	mov rsi,8
+	call print_float
+	mov rsi,.grammar+8
+	mov rdx,1
+	call print_chars
+	
+	; flush print buffer
+	call print_buffer_flush
+
+	; exit with number of secant iterations as return value
+	mov dil,al
+	call exit	
+
+.lower_bound:
+	dq -5.0
+.upper_bound:
+	dq 5.0
+.tolerance:
+	dq 0.001
+.grammar:
+	db `sqrt(2)=\n`
+
+END:
+
+PRINT_BUFFER: 	; PRINT_BUFFER_SIZE bytes will be allocated here at runtime,
+		; all initialized to zeros
---
+++ lib/math/root_finding/secant_method.asm
@@ -0,0 +1,70 @@
+%ifndef SECANT_METHOD
+%define SECANT_METHOD
+
+secant_method:
+; ulong {rax}, double {xmm0} secant_method(void* {rdi}, double {xmm0}, 
+;					double {xmm1}, double {xmm2}
+;	Uses the secant method to find a root for the single-variable 
+;	function at address {rdi} with initial guesses {xmm0} and {xmm1}
+;	to within tolerance {xmm2}. On fail, {rax}=0. On success, the 
+;	root is returned in {xmm0}, and {rax} contains the number of 
+;	iterations.
+;
+;	The function of interest at address {rdi} should be of the form:
+;		double {xmm0} func(double {xmm0});
+;	and should not affect any registers besides {xmm0}.
+
+	sub rsp,80
+	movdqu [rsp+0],xmm1
+	movdqu [rsp+16],xmm3
+	movdqu [rsp+32],xmm4
+	movdqu [rsp+48],xmm5
+	movdqu [rsp+64],xmm6
+
+	xor rax,rax		; count number of iterations in {rax}
+
+
+.loop:
+	movsd xmm3,xmm1		; (x2-x1) in {xmm3}
+	subsd xmm3,xmm0
+
+	movsd xmm6,xmm3
+	pslld xmm6,1
+	psrld xmm6,1
+	comisd xmm6,xmm2
+	jbe .ret	
+
+	call rdi	
+	movsd xmm4,xmm0		; {xmm4} = f(x1);
+
+	movsd xmm0,xmm1
+	call rdi
+	movsd xmm5,xmm0		; {xmm5} = f(x2);
+
+	mulsd xmm3,xmm5		; f(x2)*(x2-x1)
+	subsd xmm5,xmm4		; f(x2)-f(x1)
+	mulsd xmm3,xmm5		; f(x2)*(x2-x1)*(f(x2)-f(x1))
+
+	movsd xmm0,xmm1		; new x1
+	subsd xmm1,xmm5		; new x2
+
+	inc rax
+
+	jmp .loop
+
+.ret:
+	movdqu xmm1,[rsp+0]
+	movdqu xmm3,[rsp+16]
+	movdqu xmm4,[rsp+32]
+	movdqu xmm5,[rsp+48]
+	movdqu xmm6,[rsp+64]
+	add rsp,80
+
+	ret			; return
+
+.zero:
+	dq 0.0
+.half:
+	dq 0.5
+
+%endif
