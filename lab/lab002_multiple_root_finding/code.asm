6f7ece8111e2beb18239e284dc26bc997e9190f5
xmdi <xmdi@protonmail.com>
2023-10-25 22:55:40 -0400

    some :bugs: fixed, plot added but not working? issue with heap?

--- lab/lab002_multiple_root_finding/code.asm
+++ lab/lab002_multiple_root_finding/code.asm
@@ -5,7 +5,7 @@
 %define LOAD_ADDRESS 0x00020000 ; pretty much any number >0 works
 %define CODE_SIZE END-(LOAD_ADDRESS+0x78) ; everything beyond HEADER is code
 %define PRINT_BUFFER_SIZE 4096
-%define HEAP_SIZE 128
+%define HEAP_SIZE 4096
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;HEADER;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -50,6 +50,12 @@ PROGRAM_HEADER:
 
 %include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
 
+%include "lib/io/file_open.asm"
+; int {rax} file_open(char* {rdi}, int {rsi}, int {rdx});
+
+%include "lib/io/file_close.asm"
+; int {rax} file_close(int {rdi});
+
 %include "lib/mem/heap_init.asm"
 ; void heap_init(void);
 
@@ -69,6 +75,16 @@ PROGRAM_HEADER:
 %include "lib/math/root_finding/bisection_method.asm"
 ; ulong {rax}, double {xmm0} bisection_method(void* {rdi}, double {xmm0}, 
 ;					double {xmm1}, double {xmm2});
+
+%include "lib/math/parametric/evaluate_parameters.asm"
+; bool {rax} evaluate_parameters(void* {rdi}, void* {rsi}, void* {rdx});
+
+%include "lib/math/parametric/linear_space.asm"
+; bool {rax} linear_space(double* {rdi}, long {rsi}, ulong {rdx}
+;			double {xmm0}, double {xmm1});
+
+%include "lib/io/svg/scatter_plot.asm"
+; void scatter_plot(uint {rdi}, struct* {rsi});i
 
 %include "lib/sys/exit.asm"	
 ; void exit(byte {dil});
@@ -139,6 +155,7 @@ START:
 	shl rdi,3
 	call heap_alloc
 	mov r8,rax
+	mov r9,rax
 
 	; {r8} now contains the address of an array for the roots
 
@@ -164,8 +181,8 @@ START:
 	movsd xmm2,[.tolerance]
 	call bisection_method
 
-	movsd [r8],xmm0
-	add r8,8
+	movsd [r9],xmm0
+	add r9,8
 
 .no_root_detected2:
 
@@ -179,12 +196,56 @@ START:
 	; print roots
 	mov rdi,SYS_STDOUT	; STDOUT file descriptor
 	mov rsi,r8		; matrix start address
-	mov rdx,rcx		; 4 rows
-	mov rcx,1		; 4 columns
+	; {rdx} rows
+	; {rcx} columns
 	xor r8,r8		; no extra offsets betwixt elements
 	mov r9,print_float	; print without scientific notation
 	mov r10,5		; 5 significant figures
 	call print_array_float
+
+	call print_buffer_flush
+
+
+	; allocate arrays on heap
+	mov rdi,808	; 101x 8-byte doubles
+	call heap_alloc
+	mov r12,rax	; save pointer to x_array in {r12}
+	mov rdi,808	; 101x 8-byte doubles
+	call heap_alloc
+	mov r13,rax	; save pointer to y_array in {r13}
+
+	; store heap locations of arrays into structures
+	mov [.x_param+8],r12
+	mov [.scatter_dataset_structure+16],r12
+	mov [.y_param+8],r13
+	mov [.scatter_dataset_structure+26],r13
+
+	; create linear spacing in x_param
+	movsd xmm0,[.lower_bound] ; lower bound
+	movsd xmm1,[.upper_bound] ; upper bound
+	mov rdi,r12	; location of x_array on heap
+	xor rsi,rsi
+	mov rdx,101
+	call linear_space
+
+	; evaluate function of x_param into y_param
+	mov rdi,.y_param
+	mov rsi,.x_param
+	mov rdx,FUNC
+	call evaluate_parameters
+
+	; open file
+	mov rdi,.filename
+	mov rsi,SYS_READ_WRITE+SYS_CREATE_FILE+SYS_TRUNCATE
+	mov rdx,SYS_DEFAULT_PERMISSIONS
+	call file_open	; file descriptor in {rax}
+
+	; write scatter plot to SVG file
+	mov rdi,rax			; output file descriptor
+	mov rsi,.scatter_plot_structure		; structure start address
+	call scatter_plot
+
+	call file_close
 
 	; flush print buffer
 	call print_buffer_flush
@@ -203,6 +264,99 @@ START:
 	dq 0.0
 .tolerance:
 	dq 0.0001
+
+.x_param:
+	dq 0 ; address of next parameter in list (0 on last element)
+	dq 0 ; address of first parameter value
+	dq 0 ; extra stride between parameter values
+	dq 101 ; number of values (only matters for first input parameter in linked list)
+	dq 0 ; work zone (track address of current element), initial value unused
+
+.y_param:
+	dq 0 ; address of next parameter in list (0 on last element)
+	dq 0 ; address of first parameter value
+	dq 0 ; extra stride between parameter values
+	dq 101 ; number of values (only matters for first input parameter in linked list)
+	dq 0 ; work zone (track address of current element), initial value unused
+
+.filename:
+	db `sine_roots.svg`,0
+
+.scatter_title:
+	db `Roots of Sine(x)`,0
+
+.scatter_xlabel:
+	db `x`,0
+
+.scatter_ylabel:
+	db `y`,0
+
+.scatter_plot_structure:
+	dq .scatter_title; address of null-terminated title string {*+0}
+	dq .scatter_xlabel; address of null-terminated x-label string {*+8}
+	dq .scatter_ylabel; address of null-terminated y-label string {*+16}
+	dq .scatter_dataset_structure; address of linked list for datasets {*+24}
+	dw 800; plot width (px) {*+32}
+	dw 400; plot height (px) {*+34}
+	dw 5; plot margins (px) {*+36}
+	dq -12.0; x-min (double) {*+38}
+	dq 12.0; x-max (double) {*+46}
+	dq -12.0; y-min (double) {*+54}
+	dq 12.0; y-max (double) {*+62}
+	dw 0; legend left x-coordinate (px) {*+70}
+	dw 0; legend top y-coordinate (px) {*+72}
+	dw 0; legend width (px) {*+74}
+	dd 0xFFFFFF; #XXXXXX RGB background color {*+76}
+	dd 0x000000; #XXXXXX RGB axis color {*+80}
+	dd 0x000000; #XXXXXX RGB font color {*+84}
+	db 9; number of major x-ticks {*+88}
+	db 7; number of major y-ticks {*+89}
+	db 2; minor subdivisions per x-tick {*+90}
+	db 2; minor subdivisions per y-tick {*+91}
+	db 2; significant digits on x values {*+92}
+	db 2; significant digits on y values {*+93}
+	db 32; title font size (px) {*+94}
+	db 5; vertical margin below title (px) {*+95}
+	db 24; axis label font size (px) {*+96}
+	db 16; tick & legend label font size (px) {*+97}
+	db 5; horizontal margin right of y-tick labels (px) {*+98}
+	db 5; vertical margin above x-tick labels (px) {*+99}
+	db 2; grid major stroke thickness (px) {*+100}
+	db 1; grid minor stroke thickness (px) {*+101}
+	db 60; width for y-axis ticks (px) {*+102}
+	db 40; height for x-axis ticks (px) {*+103}
+	db 0x1F; flags: {*+104}
+		; bit 0 (LSB)	= show title?
+		; bit 1		= show x-label?
+		; bit 2		= show y-label?
+		; bit 3		= draw grid?
+		; bit 4		= show tick labels?
+		; bit 5		= draw legend?
+
+.scatter_dataset_structure:
+	dq 0; address of next dataset in linked list {*+0}
+	dq 0; address of null-terminated label string {*+8}
+	dq 0; address of first x-coordinate {*+16}
+	dw 0; extra stride between x-coord elements {*+24}
+	dq 0; address of first y-coordinate {*+26}
+	dw 0; extra stride between y-coord elements {*+34}
+	dd 101; number of elements {*+36}
+	dd 0xFF0000; #XXXXXX RGB marker color {*+40}
+	dd 0xFF0000; #XXXXXX RGB line color {*+44}
+	dd 0x000000; #XXXXXX RGB fill color {*+48}
+	db 5; marker size (px) {*+52}
+	db 5; line thickness (px) {*+53}
+	db 0; fill opacity (%) {*+54}
+	db 0x03; flags: {*+55}
+		; bit 0 (LSB)	= point marker?
+		; bit 1		= connecting lines?
+		; bit 2		= dashed line? (bit 1 must be set)
+		; bit 3		= fill?
+		; bit 4		= include in legend?
+		; bits 6-5	= 00 = no curves
+		;		= 01 = quadratic bezier
+		;		= 10 = cubic bezier
+		;		= 11 = arc
 
 END:
 
