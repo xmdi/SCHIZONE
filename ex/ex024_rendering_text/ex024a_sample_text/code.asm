1a575c4d10955b5fa34ba25cf2e05aa92ad0567e
xmdi <xmdi@protonmail.com>
2024-01-11 23:53:33 -0500

    spq fixes my bugs

--- ex/ex024_rendering_text/ex024a_sample_text/code.asm
+++ ex/ex024_rendering_text/ex024a_sample_text/code.asm
@@ -100,23 +100,140 @@ SCHIZOFONT:
 	db 0b00000000
 	db 0b00000000
 .octothorpe:
-	db 0b00100100
-	db 0b00100100
-	db 0b01111110
+	db 0b00000000
 	db 0b00100100
 	db 0b01111110
 	db 0b00100100
+	db 0b00100100
+	db 0b01111110
 	db 0b00100100
 	db 0b00000000
-.test:
-	db 0b11111111
-	db 0b11111111
-	db 0b11111111
-	db 0b11111111
-	db 0b11111111
-	db 0b11111111
-	db 0b11111111
-	db 0b11111111
+.money:
+	db 0b00010000
+	db 0b00111000
+	db 0b01010100
+	db 0b00110000
+	db 0b00011000
+	db 0b01010100
+	db 0b00111000
+	db 0b00010000
+.percent:
+	db 0b01100000
+	db 0b01100010
+	db 0b00000100
+	db 0b00001000
+	db 0b00010000
+	db 0b00100000
+	db 0b01000110
+	db 0b00000110
+.ampersand:
+	db 0b00110000
+	db 0b01001000
+	db 0b01001000
+	db 0b00110000
+	db 0b01010100
+	db 0b01001000
+	db 0b00110100
+	db 0b00000000
+.apostrophe:
+	db 0b00100000
+	db 0b00100000
+	db 0b00100000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+.left_parenthesis:
+	db 0b00010000
+	db 0b00100000
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b00100000
+	db 0b00010000
+	db 0b00000000
+.right_parenthesis:
+	db 0b01000000
+	db 0b00100000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00100000
+	db 0b01000000
+	db 0b00000000
+.asterisk:
+	db 0b00010000
+	db 0b01010100
+	db 0b00111000
+	db 0b01111100
+	db 0b00111000
+	db 0b01010100
+	db 0b00010000
+	db 0b00010000
+.plus:
+	db 0b00000000
+	db 0b00010000
+	db 0b00010000
+	db 0b01111100
+	db 0b00010000
+	db 0b00010000
+	db 0b00000000
+	db 0b00000000
+.comma:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00110000
+	db 0b00110000
+	db 0b01000000
+.minus:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b01111100
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+.period:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b01100000
+	db 0b01100000
+	db 0b00000000
+.forward_slash:
+	db 0b00000000
+	db 0b00000100
+	db 0b00001000
+	db 0b00010000
+	db 0b00100000
+	db 0b01000000
+	db 0b00000000
+	db 0b00000000
+.0:
+	db 0b0011100
+	db 0b01000100
+	db 0b01001100
+	db 0b01010100
+	db 0b01100100
+	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.1:
+	db 0b00010000
+	db 0b00110000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00111000
+	db 0b00000000
 .2:
 	db 0b00111000
 	db 0b01000100
@@ -125,6 +242,24 @@ SCHIZOFONT:
 	db 0b01000000
 	db 0b01000000
 	db 0b01111100
+	db 0b00000000
+.3:
+	db 0b00111000
+	db 0b01000100
+	db 0b00000100
+	db 0b00011000
+	db 0b00000100
+	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.4:
+	db 0b00001000
+	db 0b00011000
+	db 0b00101000
+	db 0b01001000
+	db 0b01111100
+	db 0b00001000
+	db 0b00001000
 	db 0b00000000
 .5:
 	db 0b01111100
@@ -142,6 +277,96 @@ SCHIZOFONT:
 	db 0b01111000
 	db 0b01000100
 	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.7:
+	db 0b01111100
+	db 0b00000100
+	db 0b00001000
+	db 0b00010000
+	db 0b00100000
+	db 0b01000000
+	db 0b01000000
+	db 0b00000000
+.8:
+	db 0b00111000
+	db 0b01000100
+	db 0b01000100
+	db 0b00111000
+	db 0b01000100
+	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.9:
+	db 0b00111000
+	db 0b01000100
+	db 0b01000100
+	db 0b00111100
+	db 0b00000100
+	db 0b00001000
+	db 0b00110000
+	db 0b00000000
+.colon:
+	db 0b00000000
+	db 0b00000000
+	db 0b00110000
+	db 0b00110000
+	db 0b00000000
+	db 0b00110000
+	db 0b00110000
+	db 0b00000000
+.semicolon:
+	db 0b00000000
+	db 0b00000000
+	db 0b00110000
+	db 0b00110000
+	db 0b00000000
+	db 0b00110000
+	db 0b00110000
+	db 0b00100000
+.less_than:
+	db 0b00001000
+	db 0b00010000
+	db 0b00100000
+	db 0b01000000
+	db 0b00100000
+	db 0b00010000
+	db 0b00001000
+	db 0b00000000
+.equals:
+	db 0b00000000
+	db 0b00000000
+	db 0b01111100
+	db 0b00000000
+	db 0b01111100
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+.greater_than:
+	db 0b01000000
+	db 0b00100000
+	db 0b00010000
+	db 0b00001000
+	db 0b00010000
+	db 0b00100000
+	db 0b01000000
+	db 0b00000000
+.question_mark:
+	db 0b00111000
+	db 0b01000100
+	db 0b00000100
+	db 0b00001000
+	db 0b00010000
+	db 0b00000000
+	db 0b00010000
+	db 0b00000000
+.at:
+	db 0b00111000
+	db 0b01000100
+	db 0b00000100
+	db 0b00110100
+	db 0b01010100
+	db 0b01010100
 	db 0b00111000
 	db 0b00000000
 .A:
@@ -378,6 +603,340 @@ SCHIZOFONT:
 	db 0b01000000
 	db 0b01111100
 	db 0b00000000
+.left_square_bracket:
+	db 0b01110000
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b01110000
+	db 0b00000000
+.back_slash:
+	db 0b00000000
+	db 0b01000000
+	db 0b00100000
+	db 0b00010000
+	db 0b00001000
+	db 0b00000100
+	db 0b00000000
+	db 0b00000000
+.right_square_bracket:
+	db 0b0111000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b01110000
+	db 0b00000000
+.caret:
+	db 0b00010000
+	db 0b00101000
+	db 0b01000100
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+.underscore:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b01111100
+	db 0b00000000
+.tick:
+	db 0b01000000
+	db 0b00100000
+	db 0b00010000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+.a:
+	db 0b00000000
+	db 0b00000000
+	db 0b00111000
+	db 0b00000100
+	db 0b00111100
+	db 0b01000100
+	db 0b00111100
+	db 0b00000000
+.b:	
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b01111000
+	db 0b01000100
+	db 0b01000100
+	db 0b01111000
+	db 0b00000000
+.c:
+	db 0b00000000
+	db 0b00000000
+	db 0b00111000
+	db 0b01000100
+	db 0b01000000
+	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.d:
+	db 0b00000100
+	db 0b00000100
+	db 0b00000100
+	db 0b00111100
+	db 0b01000100
+	db 0b01000100
+	db 0b00111100
+	db 0b00000000
+.e:
+	db 0b00000000
+	db 0b00000000
+	db 0b00111000
+	db 0b01000100
+	db 0b01111100
+	db 0b01000000
+	db 0b00111000
+	db 0b00000000
+.f:
+	db 0b00010000
+	db 0b00101000
+	db 0b00100000
+	db 0b01110000
+	db 0b00100000
+	db 0b00100000
+	db 0b00100000
+	db 0b00000000
+
+.g:
+	db 0b00000000
+	db 0b00000000
+	db 0b00111000
+	db 0b01000100
+	db 0b00111100
+	db 0b00000100
+	db 0b01000100
+	db 0b00111000
+.h:
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b01111000
+	db 0b01000100
+	db 0b01000100
+	db 0b01000100
+	db 0b00000000
+.i:
+	db 0b00000000
+	db 0b00100000
+	db 0b00000000
+	db 0b01100000
+	db 0b00100000
+	db 0b00100000
+	db 0b0111000
+	db 0b00000000
+.j:
+	db 0b00000000
+	db 0b00000100
+	db 0b00000000
+	db 0b00000100
+	db 0b00000100
+	db 0b00000100
+	db 0b01000100
+	db 0b00111000
+.k:
+	db 0b01000000
+	db 0b01000000
+	db 0b01001000
+	db 0b01010000
+	db 0b01100000
+	db 0b01010000
+	db 0b01001000
+	db 0b00000000
+.l:
+	db 0b01100000
+	db 0b00100000
+	db 0b00100000
+	db 0b00100000
+	db 0b00100000
+	db 0b00100000
+	db 0b01110000
+	db 0b00000000
+.m:
+	db 0b00000000
+	db 0b00000000
+	db 0b01101000
+	db 0b01010100
+	db 0b01010100
+	db 0b01010100
+	db 0b01010100
+	db 0b00000000
+.n:
+	db 0b00000000
+	db 0b00000000
+	db 0b01110000
+	db 0b01001000
+	db 0b01001000
+	db 0b01001000
+	db 0b01001000
+	db 0b00000000
+.o:
+	db 0b00000000
+	db 0b00000000
+	db 0b00111000
+	db 0b01000100
+	db 0b01000100
+	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.p:
+	db 0b00000000
+	db 0b00000000
+	db 0b01111000
+	db 0b01000100
+	db 0b01000100
+	db 0b01111000
+	db 0b01000000
+	db 0b01000000
+.q:
+	db 0b00000000
+	db 0b00000000
+	db 0b00111100
+	db 0b01000100
+	db 0b01000100
+	db 0b00111100
+	db 0b00000100
+	db 0b00000100
+.r:
+	db 0b00000000
+	db 0b00000000
+	db 0b01111000
+	db 0b01000100
+	db 0b01000000
+	db 0b01000000
+	db 0b01000000
+	db 0b00000000
+.s:
+	db 0b00000000
+	db 0b00000000
+	db 0b01111000
+	db 0b10000000
+	db 0b01111000
+	db 0b00000100
+	db 0b01111000
+	db 0b00000000
+.t:
+	db 0b00010000
+	db 0b00010000
+	db 0b01111100
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00001000
+	db 0b00000000
+.u:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b01000100
+	db 0b01000100
+	db 0b01000100
+	db 0b00111000
+	db 0b00000000
+.v:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b01000100
+	db 0b01000100
+	db 0b00101000
+	db 0b00010000
+	db 0b00000000
+.w:
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+	db 0b01000100
+	db 0b01010100
+	db 0b01010100
+	db 0b00101000
+	db 0b00000000
+.x:
+	db 0b00000000
+	db 0b00000000
+	db 0b01000100
+	db 0b00101000
+	db 0b00010000
+	db 0b00101000
+	db 0b01000100
+	db 0b00000000
+.y:
+	db 0b00000000
+	db 0b00000000
+	db 0b01000100
+	db 0b01000100
+	db 0b00111100
+	db 0b00000100
+	db 0b01000100
+	db 0b00111000
+.z:
+	db 0b00000000
+	db 0b00000000
+	db 0b01111100
+	db 0b00001000
+	db 0b00010000
+	db 0b00100000
+	db 0b01111100
+	db 0b00000000
+.left_curly_bracket:
+	db 0b00010000
+	db 0b00100000
+	db 0b00100000
+	db 0b01000000
+	db 0b00100000
+	db 0b00100000
+	db 0b00010000
+	db 0b00000000
+.pipe:
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00010000
+	db 0b00000000
+.right_curly_bracket:
+	db 0b01000000
+	db 0b00100000
+	db 0b00100000
+	db 0b00010000
+	db 0b00100000
+	db 0b00100000
+	db 0b01000000
+	db 0b00000000
+.tilde:
+	db 0b00000000
+	db 0b00000000
+	db 0b00100000
+	db 0b01010100
+	db 0b00001000
+	db 0b00000000
+	db 0b00000000
+	db 0b00000000
+.unknown:
+	db 0b11111111
+	db 0b11101111
+	db 0b11101111
+	db 0b10000011
+	db 0b11101111
+	db 0b11101111
+	db 0b11101111
+	db 0b11111111
 
 font_expandomatic:
 	; takes character address (64 bit character) from rax and expands each bit to a pixel, rendering it to the screen
@@ -385,6 +944,7 @@ font_expandomatic:
 		; loop thru the bits (low to high)
 	push r8
 	push r9
+	push rax
 	sub r9,8
 	mov r12,8
 .row_loop:
@@ -416,6 +976,7 @@ font_expandomatic:
 	dec r12
 	jnz .row_loop
 
+	pop rax
 	pop r9
 	pop r8
 	ret
@@ -601,6 +1162,62 @@ START:
 	mov rax,SCHIZOFONT.exclamation_mark ; put address of foreground here
 	call font_expandomatic
 
+
+	mov r8d,500
+	mov r9d,564
+	mov rax,SCHIZOFONT.s ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.p ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.q ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.c ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.i ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.v ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.i ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.t ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.a ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.t ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.u ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.m ; put address of foreground here
+	call font_expandomatic
+	add r8d,8
+	mov rax,SCHIZOFONT.money ; put address of foreground here
+	call font_expandomatic
+
+	mov r8d,100
+	mov r9d,580
+	mov r15,96
+	mov rax,SCHIZOFONT
+.test_loop:
+	call font_expandomatic
+	add r8d,8
+	add rax,8
+	dec r15
+	jnz .test_loop
+
+
+	
+	
 	call framebuffer_flush	; flush frame to framebuffer
 	
 	xor dil,dil
