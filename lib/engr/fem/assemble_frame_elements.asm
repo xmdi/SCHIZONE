6f0fc1762a8ccde6c48ce25e369c41607013c78b
xmdi <xmdi@protonmail.com>
2024-02-25 21:08:10 -0500

    K matrix doc

--- lib/engr/fem/assemble_frame_elements.asm
+++ lib/engr/fem/assemble_frame_elements.asm
@@ -21,6 +21,24 @@ assemble_frame_elements:
 		dq 0 ; pointer to stiffness matrix (K)
 		dq 0 ; pointer to known forcing array (F)
 %endif
+
+; 12x12 frame element stiffness matrix has the form:
+;	C1 = EA/L,	C2 = 12EIz/L^3,		C3 = 6EIz/L^2
+;	C4 = GL/L,	C5 = 12EIy/L^3,		C6 = 6EIy/L^2
+;	C7 = 4EIz/L,	C8 = 2EIz/L
+;	C9 = 4EIy/L,	C10 = 2EIy/L
+;  K=	[ C1 0 0 0 0 0 -C1 0 0 0 0 0 ]
+;	[ 0 C2 0 0 0 C3 0 -C2 0 0 0 C3 ]
+;	[ 0 0 C5 0 -C6 0 0 0 -C5 0 -C6 0 ]
+;	[ 0 0 0 C4 0 0 0 0 0 -C4 0 0 ]
+;	[ 0 0 -C6 0 C9 0 0 0 C6 0 C10 0 ]
+;	[ 0 C3 0 0 0 C7 0 -C3 0 0 0 C7 ]
+;	[ -C1 0 0 0 0 0 C1 0 0 0 0 0 ]
+;	[ 0 -C2 0 0 0 -C3 0 C2 0 0 0 -C3 ]
+;	[ 0 0 -C5 0 C6 0 0 0 C5 0 C6 0 ]
+;	[ 0 0 0 -C4 0 0 0 0 0 C4 0 0 ]
+;	[ 0 0 -C6 0 C10 0 0 0 C6 0 C9 0 ]
+;	[ 0 C3 0 0 0 C8 0 -C3 0 0 0 C7 ]
 	
 	ret			; return
 
