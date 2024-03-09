6c2c1b061c1c3dfd0a9e0506f0c314fef25cebed
xmdi <xmdi@protonmail.com>
2024-03-08 17:15:25 -0800

    dome prob with :bug:s

--- lib/math/integration/trapezoidal_method.asm
+++ lib/math/integration/trapezoidal_method.asm
@@ -6,10 +6,11 @@ trapezoidal_method:
 ; Estimates the definite integral of the function at address {rdi} between {xmm0}<=x<={xmm1} using a trapezoidal method and step size in {xmm2}. Area returned in {xmm0}.
 ; Function of interest should take independent variable and returns dependend variable in {xmm0}.
 
-	sub rsp,48
+	sub rsp,64
 	movdqu [rsp+0],xmm3
 	movdqu [rsp+16],xmm4
 	movdqu [rsp+32],xmm5
+	movdqu [rsp+48],xmm6
 
 	movsd xmm3,xmm0		; x val track
 	pxor xmm4,xmm4		; track sum
@@ -21,20 +22,24 @@ trapezoidal_method:
 .loop:
 	movsd xmm0,xmm3
 	call rdi
+	movsd xmm6,xmm0
 	addsd xmm0,xmm5
 	mulsd xmm0,[.half]
 	mulsd xmm0,xmm2
 	addsd xmm4,xmm0
 
-	movsd xmm5,xmm0;;;;fix
+	movsd xmm5,xmm6
 	addsd xmm3,xmm2
 	comisd xmm3,xmm1
 	jbe .loop
 
 .ret:
+	movsd xmm0,xmm4
 	movdqu xmm3,[rsp+0]
 	movdqu xmm4,[rsp+16]
-	add rsp,48
+	movdqu xmm5,[rsp+32]
+	movdqu xmm6,[rsp+48]
+	add rsp,64
 
 	ret		; return
 
