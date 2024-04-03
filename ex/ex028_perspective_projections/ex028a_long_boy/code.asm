1eb626e5ca404e683e6bb9ef1b82fc99cef5513c
xmdi <xmdi@protonmail.com>
2024-04-02 23:58:21 -0400

    change ex

--- ex/ex028_perspective_projections/ex028a_long_boy/code.asm
+++ ex/ex028_perspective_projections/ex028a_long_boy/code.asm
@@ -5,6 +5,7 @@
 %define LOAD_ADDRESS 0x00020000 ; pretty much any number >0 works
 %define CODE_SIZE END-(LOAD_ADDRESS+0x78) ; everything beyond HEADER is code
 %define PRINT_BUFFER_SIZE 4096
+%define HEAP_SIZE 0x2000000 ; ~32 MB
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;HEADER;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -21,7 +22,7 @@ ELF_HEADER:
 	db 0x00 ; ABI version (ignored?)
 	times 7 db 0x00 ; 7 padding bytes
 	dw 0x0002 ; executable file
-	dw 0x003E ; AMD x86-64
+	dw 0x003E ; AMD x86-64 
 	dd 0x00000001 ; version 1
 	dq START ; entry point for our program
 	dq 0x0000000000000040 ; 0x40 offset from ELF_HEADER to PROGRAM_HEADER
@@ -40,7 +41,7 @@ PROGRAM_HEADER:
 	dq LOAD_ADDRESS+0x78 ; virtual address of segment in memory
 	dq 0x0000000000000000 ; physical address of segment in memory (ignored?)
 	dq CODE_SIZE ; size (bytes) of segment in file image
-	dq CODE_SIZE+PRINT_BUFFER_SIZE ; size (bytes) of segment in memory
+	dq CODE_SIZE+PRINT_BUFFER_SIZE+HEAP_SIZE ; size (bytes) of segment in memory
 	dq 0x0000000000000000 ; alignment (doesn't matter, only 1 segment)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -49,25 +50,231 @@ PROGRAM_HEADER:
 
 %include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
 
-%include "lib/io/print_float.asm"
-; void print_float(int {rdi}, double {xmm0}, int {rsi});
+%include "lib/io/framebuffer/framebuffer_3d_render_init.asm"
 
-%include "lib/io/print_int_d.asm"
-; void print_int_d(int {rdi}, long {rsi});
-
-%include "lib/sys/exit.asm"
-; void exit(char {dil});
+%include "lib/io/framebuffer/framebuffer_3d_render_loop.asm"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; NOTE: NEED TO RUN THIS AS SUDO
+
+DRAW_CROSS_CURSOR:
+;	inputs:
+; 	{rdi}=framebuffer_address
+;	{rsi}=color
+	mov rsi,0x1FFFFFF00
+;	{edx}=framebuffer_width
+;	{ecx}=framebuffer_height
+;	{r8d}=mouse_x
+;	{r9d}=mouse_y
+	
+	push r8
+	push r9
+	push r10
+	push r11
+
+	mov r10,r8
+	sub r8,7
+	add r10,7
+	mov r11,r9
+	call set_line
+	
+	mov r8,[rsp+24]
+	mov r10,r8
+	mov r11,r9
+	add r9,14
+	sub r11,7
+	call set_line
+
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	ret
+
 START:
 
-	xor dil,dil
-	call exit
+	mov rdi,.perspective_structure
+	mov rsi,.faces_geometry
+	mov rdx,DRAW_CROSS_CURSOR
+	call framebuffer_3d_render_init
+
+.loop:
+	call framebuffer_3d_render_loop
+	jmp .loop
+
+.perspective_structure:
+	dq 3.00 ; lookFrom_x	
+	dq 2.00 ; lookFrom_y	
+	dq 2.00 ; lookFrom_z	
+	dq 0.00 ; lookAt_x	
+	dq 0.00 ; lookAt_y	
+	dq 2.00 ; lookAt_z	
+	dq 0.0 ; upDir_x	
+	dq 0.0 ; upDir_y	
+	dq 1.0 ; upDir_z	
+	dq 0.3	; zoom
+
+.faces_geometry:
+	dq 0 ; next geometry in linked list
+	dq .shell_list_structure ; address of point/edge/face structure
+	dq 0x100000000 ; color (0xARGB)
+	db 0b00000101 ; type of structure to render
+
+.shell_list_structure:
+	dq 4 ; number of structures
+	dq .faces_top_structure ; address of shell body structure
+	dq 0.0, 0.0, 3.5 ; centroid of shell body structure
+	dq .faces_bottom_structure ; address of shell body structure
+	dq 0.0, 0.0, 1.0 ; centroid of shell body structure
+	dq .faces_right_structure ; address of shell body structure
+	dq -1.0, 0.0, 2.5 ; centroid of shell body structure
+	dq .faces_left_structure ; address of shell body structure
+	dq 1.0, 0.0, 2.5 ; centroid of shell body structure
+
+.faces_top_structure:
+	dq 24 ; number of points (N)
+	dq 10 ; number of faces (M)
+	dq .points ; starting address of point array (3N elements)
+	dq .faces_top ; starting address of face array 
+		;	(3M elements if no colors)
+		;	(4M elements if colors)
+
+.faces_bottom_structure:
+	dq 24 ; number of points (N)
+	dq 10 ; number of faces (M)
+	dq .points ; starting address of point array (3N elements)
+	dq .faces_bottom ; starting address of face array 
+		;	(3M elements if no colors)
+		;	(4M elements if colors)
+
+.faces_right_structure:
+	dq 24 ; number of points (N)
+	dq 10 ; number of faces (M)
+	dq .points ; starting address of point array (3N elements)
+	dq .faces_right ; starting address of face array 
+		;	(3M elements if no colors)
+		;	(4M elements if colors)
+
+.faces_left_structure:
+	dq 24 ; number of points (N)
+	dq 10 ; number of faces (M)
+	dq .points ; starting address of point array (3N elements)
+	dq .faces_left ; starting address of face array 
+		;	(3M elements if no colors)
+		;	(4M elements if colors)
+
+.points:
+	; base of vertical beam
+	dq 0.5,0.5,0.0
+	dq -0.5,0.5,0.0
+	dq -0.5,-0.5,0.0
+	dq 0.5,-0.5,0.0
+
+	; bottom of cross beam
+	dq 0.5,0.5,2.0
+	dq -0.5,0.5,2.0
+	dq -0.5,-0.5,2.0
+	dq 0.5,-0.5,2.0
+
+	; top of cross beam
+	dq 0.5,0.5,3.0
+	dq -0.5,0.5,3.0
+	dq -0.5,-0.5,3.0
+	dq 0.5,-0.5,3.0
+
+	; top of vertical beam
+	dq 0.5,0.5,4.0
+	dq -0.5,0.5,4.0
+	dq -0.5,-0.5,4.0
+	dq 0.5,-0.5,4.0
+
+	; left side of cross beam
+	dq 1.5,0.5,2.0
+	dq 1.5,-0.5,2.0
+	dq 1.5,-0.5,3.0
+	dq 1.5,0.5,3.0
+
+	; right side of cross beam
+	dq -1.5,0.5,2.0
+	dq -1.5,-0.5,2.0
+	dq -1.5,-0.5,3.0
+	dq -1.5,0.5,3.0
+
+.faces_top:
+	
+	dq 13,14,12,0x1FF0000FF ; top
+	dq 14,15,12,0x1FF0000FF ; top
+
+	dq 8,12,15,0x1FFFF00FF ; top left
+	dq 8,15,11,0x1FFFF00FF ; top left
+	
+	dq 9,14,13,0x1FFFFFF00 ; top right
+	dq 9,10,14,0x1FFFFFF00 ; top right
+	
+	dq 9,12,8,0x1FF00FF00 ; top front
+	dq 9,13,12,0x1FF00FF00 ; top front
+
+	dq 11,15,10,0x1FFFFFFFF ; top back
+	dq 10,15,14,0x1FFFFFFFF ; top back
+
+.faces_bottom:
+
+	dq 0,2,1,0x1FFFF0000 ; bottom
+	dq 0,3,2,0x1FFFF0000 ; bottom
+
+	dq 0,7,3,0x1FFFF00FF ; bottom left
+	dq 0,4,7,0x1FFFF00FF ; bottom left
+	
+	dq 2,6,5,0x1FFFFFF00 ; bottom right
+	dq 2,5,1,0x1FFFFFF00 ; bottom right
+
+	dq 0,9,8,0x1FF00FF00 ; top front
+	dq 0,1,9,0x1FF00FF00 ; top front
+
+	dq 3,10,2,0x1FFFFFFFF ; top back
+	dq 3,11,10,0x1FFFFFFFF ; top back
+
+.faces_right:
+
+	dq 5,21,20,0x1FFFF0000 ; bottom left
+	dq 5,6,21,0x1FFFF0000 ; bottom left
+	
+	dq 9,23,22,0x1FF0000FF ; top left
+	dq 9,22,10,0x1FF0000FF ; top left
+
+	dq 5,23,9,0x1FF00FF00 ; front right	
+	dq 5,20,23,0x1FF00FF00 ; front right	
+
+	dq 6,22,21,0x1FFFFFFFF ; back right
+	dq 6,10,22,0x1FFFFFFFF ; back right
+
+	dq 20,22,23,0x1FFFFFF00 ; right
+	dq 20,21,22,0x1FFFFFF00 ; right
+
+.faces_left:
+
+	dq 17,7,16,0x1FFFF0000 ; bottom right
+	dq 16,7,4,0x1FFFF0000 ; bottom right
+
+	dq 11,18,19,0x1FF0000FF ; top right
+	dq 11,19,8,0x1FF0000FF ; top right
+
+	dq 4,8,19,0x1FF00FF00 ; front left	
+	dq 4,19,16,0x1FF00FF00 ; front left	
+	
+	dq 7,18,11,0x1FFFFFFFF ; back left
+	dq 7,17,18,0x1FFFFFFFF ; back left
+	
+	dq 16,18,17,0x1FFFF00FF ; left
+	dq 16,19,18,0x1FFFF00FF ; left
 
 END:
 
 PRINT_BUFFER: 	; PRINT_BUFFER_SIZE bytes will be allocated here at runtime,
 		; all initialized to zeros
+
+HEAP_START_ADDRESS equ (PRINT_BUFFER+PRINT_BUFFER_SIZE)
+
