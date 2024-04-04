a3951580f8f4d10f923152d87fd61f35162bc117
xmdi <xmdi@protonmail.com>
2024-04-03 23:24:14 -0400

    correct ex

--- ex/ex028_perspective_projections/ex028a_long_boy/code.asm
+++ ex/ex028_perspective_projections/ex028a_long_boy/code.asm
@@ -50,9 +50,9 @@ PROGRAM_HEADER:
 
 %include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
 
-%include "lib/io/framebuffer/framebuffer_3d_render_init.asm"
+%include "lib/io/framebuffer/perspective/framebuffer_3d_render_depth_init.asm"
 
-%include "lib/io/framebuffer/framebuffer_3d_render_loop.asm"
+;%include "lib/io/framebuffer/perspective/framebuffer_3d_render_depth_loop.asm"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -99,16 +99,16 @@ START:
 	mov rdi,.perspective_structure
 	mov rsi,.faces_geometry
 	mov rdx,DRAW_CROSS_CURSOR
-	call framebuffer_3d_render_init
+	call framebuffer_3d_render_depth_init
 
 .loop:
-	call framebuffer_3d_render_loop
+;	call framebuffer_3d_render_loop
 	jmp .loop
 
 .perspective_structure:
-	dq 3.00 ; lookFrom_x	
+	dq 1.00 ; lookFrom_x	
 	dq 2.00 ; lookFrom_y	
-	dq 2.00 ; lookFrom_z	
+	dq 5.00 ; lookFrom_z	
 	dq 0.00 ; lookAt_x	
 	dq 0.00 ; lookAt_y	
 	dq 2.00 ; lookAt_z	
@@ -119,50 +119,15 @@ START:
 
 .faces_geometry:
 	dq 0 ; next geometry in linked list
-	dq .shell_list_structure ; address of point/edge/face structure
+	dq .faces_structure ; address of point/edge/face structure
 	dq 0x100000000 ; color (0xARGB)
-	db 0b00000101 ; type of structure to render
+	db 0b00000100 ; type of structure to render
 
-.shell_list_structure:
-	dq 4 ; number of structures
-	dq .faces_top_structure ; address of shell body structure
-	dq 0.0, 0.0, 3.5 ; centroid of shell body structure
-	dq .faces_bottom_structure ; address of shell body structure
-	dq 0.0, 0.0, 1.0 ; centroid of shell body structure
-	dq .faces_right_structure ; address of shell body structure
-	dq -1.0, 0.0, 2.5 ; centroid of shell body structure
-	dq .faces_left_structure ; address of shell body structure
-	dq 1.0, 0.0, 2.5 ; centroid of shell body structure
-
-.faces_top_structure:
+.faces_structure:
 	dq 24 ; number of points (N)
-	dq 10 ; number of faces (M)
+	dq 36 ; number of faces (M)
 	dq .points ; starting address of point array (3N elements)
-	dq .faces_top ; starting address of face array 
-		;	(3M elements if no colors)
-		;	(4M elements if colors)
-
-.faces_bottom_structure:
-	dq 24 ; number of points (N)
-	dq 10 ; number of faces (M)
-	dq .points ; starting address of point array (3N elements)
-	dq .faces_bottom ; starting address of face array 
-		;	(3M elements if no colors)
-		;	(4M elements if colors)
-
-.faces_right_structure:
-	dq 24 ; number of points (N)
-	dq 10 ; number of faces (M)
-	dq .points ; starting address of point array (3N elements)
-	dq .faces_right ; starting address of face array 
-		;	(3M elements if no colors)
-		;	(4M elements if colors)
-
-.faces_left_structure:
-	dq 24 ; number of points (N)
-	dq 10 ; number of faces (M)
-	dq .points ; starting address of point array (3N elements)
-	dq .faces_left ; starting address of face array 
+	dq .faces ; starting address of face array 
 		;	(3M elements if no colors)
 		;	(4M elements if colors)
 
@@ -191,85 +156,72 @@ START:
 	dq -0.5,-0.5,4.0
 	dq 0.5,-0.5,4.0
 
-	; left side of cross beam
+	; right side of cross beam
 	dq 1.5,0.5,2.0
 	dq 1.5,-0.5,2.0
 	dq 1.5,-0.5,3.0
 	dq 1.5,0.5,3.0
 
-	; right side of cross beam
+	; left side of cross beam
 	dq -1.5,0.5,2.0
 	dq -1.5,-0.5,2.0
 	dq -1.5,-0.5,3.0
 	dq -1.5,0.5,3.0
 
-.faces_top:
-	
-	dq 13,14,12,0x1FF0000FF ; top
-	dq 14,15,12,0x1FF0000FF ; top
-
-	dq 8,12,15,0x1FFFF00FF ; top left
-	dq 8,15,11,0x1FFFF00FF ; top left
-	
-	dq 9,14,13,0x1FFFFFF00 ; top right
-	dq 9,10,14,0x1FFFFFF00 ; top right
-	
-	dq 9,12,8,0x1FF00FF00 ; top front
-	dq 9,13,12,0x1FF00FF00 ; top front
-
-	dq 11,15,10,0x1FFFFFFFF ; top back
-	dq 10,15,14,0x1FFFFFFFF ; top back
-
-.faces_bottom:
-
+.faces:
 	dq 0,2,1,0x1FFFF0000 ; bottom
 	dq 0,3,2,0x1FFFF0000 ; bottom
-
-	dq 0,7,3,0x1FFFF00FF ; bottom left
-	dq 0,4,7,0x1FFFF00FF ; bottom left
-	
-	dq 2,6,5,0x1FFFFFF00 ; bottom right
-	dq 2,5,1,0x1FFFFFF00 ; bottom right
-
-	dq 0,9,8,0x1FF00FF00 ; top front
-	dq 0,1,9,0x1FF00FF00 ; top front
-
-	dq 3,10,2,0x1FFFFFFFF ; top back
-	dq 3,11,10,0x1FFFFFFFF ; top back
-
-.faces_right:
-
-	dq 5,21,20,0x1FFFF0000 ; bottom left
-	dq 5,6,21,0x1FFFF0000 ; bottom left
-	
-	dq 9,23,22,0x1FF0000FF ; top left
-	dq 9,22,10,0x1FF0000FF ; top left
-
-	dq 5,23,9,0x1FF00FF00 ; front right	
-	dq 5,20,23,0x1FF00FF00 ; front right	
-
-	dq 6,22,21,0x1FFFFFFFF ; back right
-	dq 6,10,22,0x1FFFFFFFF ; back right
-
-	dq 20,22,23,0x1FFFFFF00 ; right
-	dq 20,21,22,0x1FFFFFF00 ; right
-
-.faces_left:
 
 	dq 17,7,16,0x1FFFF0000 ; bottom right
 	dq 16,7,4,0x1FFFF0000 ; bottom right
 
+	dq 5,21,20,0x1FFFF0000 ; bottom left
+	dq 5,6,21,0x1FFFF0000 ; bottom left
+	
+	dq 13,14,12,0x1FF0000FF ; top
+	dq 14,15,12,0x1FF0000FF ; top
+
 	dq 11,18,19,0x1FF0000FF ; top right
 	dq 11,19,8,0x1FF0000FF ; top right
+
+	dq 9,23,22,0x1FF0000FF ; top left
+	dq 9,22,10,0x1FF0000FF ; top left
+
+	dq 0,13,12,0x1FF00FF00 ; front
+	dq 0,1,13,0x1FF00FF00 ; front
+
+	dq 5,23,9,0x1FF00FF00 ; front right	
+	dq 5,20,23,0x1FF00FF00 ; front right	
 
 	dq 4,8,19,0x1FF00FF00 ; front left	
 	dq 4,19,16,0x1FF00FF00 ; front left	
 	
+	dq 3,14,2,0x1FFFFFFFF ; back
+	dq 3,15,14,0x1FFFFFFFF ; back
+
 	dq 7,18,11,0x1FFFFFFFF ; back left
 	dq 7,17,18,0x1FFFFFFFF ; back left
 	
+	dq 6,22,21,0x1FFFFFFFF ; back right
+	dq 6,10,22,0x1FFFFFFFF ; back right
+	
 	dq 16,18,17,0x1FFFF00FF ; left
 	dq 16,19,18,0x1FFFF00FF ; left
+	
+	dq 8,12,15,0x1FFFF00FF ; top left
+	dq 8,15,11,0x1FFFF00FF ; top left
+	
+	dq 0,7,3,0x1FFFF00FF ; bottom left
+	dq 0,4,7,0x1FFFF00FF ; bottom left
+	
+	dq 20,22,23,0x1FFFFFF00 ; right
+	dq 20,21,22,0x1FFFFFF00 ; right
+	
+	dq 9,14,13,0x1FFFFFF00 ; top right
+	dq 9,10,14,0x1FFFFFF00 ; top right
+	
+	dq 2,6,5,0x1FFFFFF00 ; bottom right
+	dq 2,5,1,0x1FFFFFF00 ; bottom right
 
 END:
 
