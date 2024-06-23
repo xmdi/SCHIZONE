1d5c29db703333ebfa40d63112471579a78a02b2
xmdi <xmdi@protonmail.com>
2024-06-23 03:37:53 +0000

    ok nice output

--- ex/ex031_trig_revisited/ex031a_sine_algs/code.asm
+++ ex/ex031_trig_revisited/ex031a_sine_algs/code.asm
@@ -56,11 +56,15 @@ PROGRAM_HEADER:
 %include "lib/time/tick_time.asm" 
 %include "lib/time/tock_time.asm" 
 
+%include "lib/time/tick_cycles.asm" 
+%include "lib/time/tock_cycles.asm" 
+
 %include "lib/math/expressions/trig/sine.asm"
 %include "lib/math/expressions/trig/cosine.asm"
 
 %include "lib/io/print_string.asm"
 %include "lib/io/print_float.asm"
+%include "lib/io/print_float_scientific.asm"
 %include "lib/io/print_int_d.asm"
 
 %include "lib/sys/exit.asm"
@@ -78,6 +82,9 @@ PROGRAM_HEADER:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 START:
+	mov rsi,.explanation
+	mov rdx,.explanation_end-.explanation
+	call print_chars
 
 	; populate our random numbers
 	mov rdi,.rands
@@ -114,6 +121,39 @@ START:
 	mov rdx,1
 	call print_chars
 
+	mov r14,.rands
+	mov r13,.exacts
+	movsd xmm1,[.tight_tol]
+
+	mov rdi,SYS_STDOUT
+	mov rsi,.exact
+	mov rdx,10
+	call print_chars
+
+.exact_loop:
+
+	movsd xmm0,[r14]
+	call sine
+	movsd [r13],xmm0
+
+	mov rdi,SYS_STDOUT
+	mov rsi,6
+	call print_float
+
+	mov rsi,.grammar
+	mov rdx,3
+	call print_chars
+
+	add r14,8
+	add r13,8
+	cmp r14,.rands+40
+	jl .exact_loop
+
+	mov rsi,.grammar+5
+	mov rdx,1
+	call print_chars
+	call print_chars
+
 	mov r15,.func_table
 
 .func_loop:
@@ -126,12 +166,12 @@ START:
 	mov rdx,3
 	call print_chars
 
+	mov r13,.exacts
 	mov r14,.rands
 	add r15,8
 	mov rcx,5
 
 .value_loop:
-
 
 	movsd xmm0,[r14]
 	call [r15]
@@ -145,24 +185,51 @@ START:
 	call print_chars
 
 	add r14,8
-;	cmp r14,.rands_end
 	dec rcx
 	jnz .value_loop
 
+	mov rdi,SYS_STDOUT
+	mov rsi,.error
+	mov rdx,11
+	call print_chars
+
+	mov r13,.exacts
+	mov r14,.rands
+	mov rcx,5
+
+.value_loop2:
+
+	movsd xmm0,[r14]
+	call [r15]
+	subsd xmm0,[r13]
+
+	mov rdi,SYS_STDOUT
+	mov rsi,3
+	call print_float_scientific
+
+	mov rsi,.grammar
+	mov rdx,3
+	call print_chars
+
+	add r14,8
+	add r13,8
+	dec rcx
+	jnz .value_loop2
+
+	call tick_cycles
 	call tick_time
 	mov rcx,[.n_calls]
 	sub rcx,5
 
 .time_loop:
-;	movsd xmm0,[.bounds]
-;	movsd xmm1,[.bounds+8]
-;	call rand_float
 	movsd xmm0,[r14]
 	call [r15]
 	add r14,8
 	dec rcx
 	jnz .time_loop
 
+	call tock_cycles
+	mov rbx,rax
 	call tock_time
 	mov rdi,SYS_STDOUT
 	mov rsi,rax
@@ -171,6 +238,19 @@ START:
 	mov rsi,.grammar+2
 	mov rdx,4
 	call print_chars
+
+	xor rdx,rdx
+	mov rax,rbx
+	mov rcx,[.n_calls]
+	sub rcx,5
+	div rcx
+
+	mov rsi,rax
+	call print_int_d
+	
+	mov rsi,.grammar+6
+	mov rdx,.grammar_end-(.grammar+6)
+	call print_chars
 	
 	call print_buffer_flush
 
@@ -178,10 +258,8 @@ START:
 	cmp r15,.func_table_end
 	jl .func_loop
 
-
 	xor dil,dil
 	call exit
-
 
 align 8
 .rands:
@@ -207,20 +285,39 @@ align 8
 	dq SINE_FUNC_3
 	db `Bhaskra`,0
 	dq SINE_FUNC_4
-	db `hrdware`,0
+	db `hardwre`,0
 	dq SINE_FUNC_5
 ;	db `chbshev`,0
 ;	dq SINE_FUNC_6
-	db `cordic `,0
+	db `CORDIC `,0
 	dq SINE_FUNC_7
 
 .func_table_end:
 
+.tight_tol:
+	dq 0.00000001
+
+.exact:
+	db `exact   | `
+
+.error:
+	db `\nerror   | `
+
+.explanation:
+	db `comparison of different sine(x) approximations\n\n`
+.explanation_end:
+
 .grammar:
 	db ` | us\n`
+	db ` cycles per calculation\n\n`
+.grammar_end:
 
 .more_grammar:
 	db `x=      | `
+
+align 8
+.exacts:
+	times 5 dq 0
 
 align 64
 SINE_FUNC_1:
