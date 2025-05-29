9b60d33ece3098fae9bca62244c0902169b0ec65
xmdi <xmdi@protonmail.com>
2025-05-28 21:59:40 -0400

    binary extractor

--- aarch64/ex/ex002_basic_functions/ex002c_extract_bin/run.sh
+++ aarch64/ex/ex002_basic_functions/ex002c_extract_bin/run.sh
@@ -1,6 +1,5 @@
-touch test.file
-
-as code.asm -o temp -I ../../../lib # assemble the code into object file
+#as code.asm -o temp -I ../../../lib # assemble the code into object file
+as code.asm -o temp -I ../../../lib --defsym VERBOSE_LOGS=1 # assemble but verbose
 
 bytes=$(xxd -p -l8 -g4 -s 160 -e temp)
 bytes_trimmed="${bytes:19:8}${bytes:10:8}"
@@ -8,6 +7,7 @@ bytes_to_end=$((16#${bytes_trimmed}+120)) # extract code size from binary
 
 xxd -p -s 64 -l "$bytes_to_end" temp | xxd -r -p > binary # cut off the boomer-bytes
 
-rm temp* # remove temporary boomer object file
+#rm temp* # remove temporary boomer object file
 chmod +x binary # make binary executable
-./binary # execute binary
+
+./binary temp binary_extractor # use the binary extractor to create the binary extractor (wow epic)
